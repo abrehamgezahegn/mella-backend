2f6e9aee2bd646ea417e2b3f53aa03970375e0b0
abreham gezahegn <abreham0913@gmail.com>
2019-11-21 00:01:12 +0300

    (feat) token based authorization added

--- src/middlewares/authorize.js
+++ src/middlewares/authorize.js
@@ -1,9 +1,18 @@
-const authorize = (req, res, next) => {
-  const token = req.header("auth_token");
-  if (!token) return res.status(401).send("You are unauthorized baba :).");
+const jwt = require("jsonwebtoken");
 
-  req.body.user = { juke: "ross" };
-  next();
+const authorize = (req, res, next) => {
+  try {
+    const token = req.header("auth_token");
+    if (!token) return res.status(401).send("You are unauthorized baba :).");
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.body.token_body = decoded;
+
+    next();
+  } catch (err) {
+    res.status(500).send({ message: "Something went wrong", error: err });
+  }
 };
 
 module.exports = authorize;
